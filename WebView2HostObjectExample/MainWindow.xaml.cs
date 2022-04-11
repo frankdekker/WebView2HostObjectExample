649acbbdba787c2e9f6620f83feed280db313bc3
Frank Dekker <fdekker@123inkt.nl>
2022-04-11 21:57:01 +0200

    WebView2HostObject crash example

---
+++ WebView2HostObjectExample/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Reflection;
+using Microsoft.Web.WebView2.Core;
+
+namespace WebView2HostObjectExample
+{
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WebView.CoreWebView2InitializationCompleted += WebView2_CoreWebView2Ready;
+            OpenContent();
+        }
+
+        private void WebView2_CoreWebView2Ready(object? sender, CoreWebView2InitializationCompletedEventArgs e)
+        {
+            WebView.CoreWebView2.AddHostObjectToScript("bridge", new WebViewHostModel());
+        }
+
+        private void OpenContent()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "WebView2HostObjectExample.test.html";
+
+            var result = "";
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                result = reader.ReadToEnd();
+            }
+            
+            WebView.Dispatcher.Invoke(async () =>
+            {
+                await WebView.EnsureCoreWebView2Async().ConfigureAwait(true);
+                WebView.CoreWebView2.NavigateToString(result);
+            });
+        }
+    }
+}
---
+++ WebView2HostObjectExample/WebViewHostModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace WebView2HostObjectExample;
+
+/**
+ * @see https://docs.microsoft.com/en-us/dotnet/api/microsoft.web.webview2.core.corewebview2.addhostobjecttoscript?view=webview2-dotnet-1.0.1189-prerelease
+ */
+[ClassInterface(ClassInterfaceType.AutoDual)]
+[ComVisible(true)]
+public class WebViewHostModel
+{
+    public void MyFunction()
+    {
+        Debug.WriteLine("test");
+    }
+}
